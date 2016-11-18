d3c7cb947b335b4e7099e03f25dc70dab434626b
jamiedevivoo <JamieDeVivo@MacBook.local>
2016-11-18 01:38:36 +0000

    Tidied html and started story interface.

--- js/script.js
+++ js/script.js
@@ -2,17 +2,21 @@
 $(document).ready(function(){  // Makes sure nothing is done until the 'DOM' (Document Object Model) has loaded
 
 	// ------------------------------------------------------------------------------  FUNCTIONS
-		
+	
 					
 		function fadeUi() {
 			$('.ui').fadeIn(1000);									// Fade user interface in
 			$('.story').each(function(index) {						// For each instance of the class '.story'
 				$(this).delay(1500*index).fadeIn(1500);				// Fade in after a delay of 1500 multiplied by whatever instance of the class it is in the DOM flow (so they fade in one after another)
 			});	
+			$('.story h3').each(function(index) {	
+				$(this).delay(1500*index).fadeIn(1500);
+			});	
+			return false;
 		}
 		
 		function intro() {
-			$('.intro h1, .intro h2').fadeIn(1000, function() {
+			$('.intro h1, .intro h2, .intro h3').fadeIn(1000, function() {
 				$('.intro p').fadeIn(1000);
 			});
 		}
@@ -28,8 +32,8 @@ $(document).ready(function(){  // Makes sure nothing is done until the 'DOM' (Do
 			if (eachImage > 4) {			// if eachImage is over 4, start again.
 				eachImage = 0;
 			} else {												
-				var posX = Math.floor(Math.random() * 20) + 1;		// Choose random position between 1 and 80w/60h.
-				var posY = Math.floor(Math.random() * 20) + 1;
+				var posX = Math.floor(Math.random() * 50) + 1;		// Choose random position between 1 and 80w/60h.
+				var posY = Math.floor(Math.random() * 10) + 1;
 				
 				$('.introImage').css('left', posX + "vw");			// move image to that position
 				$('.introImage').css('top', posY + "vh");
@@ -62,8 +66,8 @@ $(document).ready(function(){  // Makes sure nothing is done until the 'DOM' (Do
 			clearInterval(pulsingInterval);
 			clearInterval(introImagesInterval);
 			
-			$('.skipIntro a').css('background-color','rgba(0,0,0,0.5');
-			$('.skipIntro a').hover(function() {
+			$('a.skipIntro').css('background-color','rgba(0,0,0,0.5');
+			$('a.skipIntro').hover(function() {
 				$(this).css('background-color','rgba(0,0,0,0.9');
 			}, function(){
 				$(this).css("background-color", 'rgba(0,0,0,0.5'); 
@@ -80,7 +84,7 @@ $(document).ready(function(){  // Makes sure nothing is done until the 'DOM' (Do
 						});
 						$('.newmap').delay(1000).fadeOut(8000, function(){
 							$('.timetravel h2').fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500, function() {
-								$('.timetravel, .skipIntro').fadeOut(800, function() {
+								$('.timetravel, a.skipIntro').fadeOut(800, function() {
 									$('.newmap').addClass('animate');
 									$('.oldmap').addClass('animate');
 									fadeUi();
@@ -96,12 +100,12 @@ $(document).ready(function(){  // Makes sure nothing is done until the 'DOM' (Do
 	// ------------------------------------------------------------------------------  SKIP INTRO BUTTON
 				
 			
-    	$(".skipIntro").click(function(){
+    	$("a.skipIntro").click(function(){
         	$('*').finish().finish().finish().finish();
 			$('.oldmap').css('display', 'none');
 			$('.ui').css('display', 'none');
 			
-			$(".skipIntro").fadeOut(500, fadeStories());
+			$("a.skipIntro").fadeOut(500, fadeStories());
     	});
 
 		
@@ -109,26 +113,39 @@ $(document).ready(function(){  // Makes sure nothing is done until the 'DOM' (Do
 	
 		var zoomed = 0; 
 	
-		$(".story").click(function() {
+		$(".story h3").click(function() {
+			
+			$('.tip').fadeOut(500);
 				
 			if  (zoomed === 0) {
 				
 				var percX = event.pageX / $('.oldmap').width() * 100;
 				var percY = event.pageY / $('.oldmap').height() * 100;
 				
+				$('.story h3').css('display','none');
 				$('.oldmap').css("box-shadow", "0px 0px 0px #000 inset,0px 0px 0px #000 inset");
 				$('.oldmap').css("background-position", percX+"% " + percY + "% ");
 				$('.oldmap').css("background-size", "200% 200%");
+				$(this).siblings(".fullStory").fadeIn(500);
 				zoomed = 1;
 				
 			} else {
-				
+				$(this).siblings(".fullStory").fadeOut(500);
 				$('.oldmap').css("background-position", "100% 50%");
 				$('.oldmap').css("background-size", "100% 100%");
 				$('.oldmap').css("box-shadow", "40px 40px 80px #000 inset,-40px -40px 80px #000 inset");
+				$('.story h3').fadeIn(100);
 				zoomed = 0;
-				
 			}
+		});
+		
+		$('a.back2Map').click(function() {
+			$(".fullStory").fadeOut(500);
+			$('.oldmap').css("background-position", "100% 50%");
+			$('.oldmap').css("background-size", "100% 100%");
+			$('.oldmap').css("box-shadow", "40px 40px 80px #000 inset,-40px -40px 80px #000 inset");
+			$('.story h3').fadeIn(500);
+			zoomed = 0;				
 		});
 		
 	// ------------------------------------------------------------------------------  SCROLL FUNCTIONS
