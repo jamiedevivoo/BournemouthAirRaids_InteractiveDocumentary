ae02b131a3a5c2c57fcec6747496f40db50777a8
jamiedevivoo <JamieDeVivo@MacBook.local>
2017-01-17 01:59:02 +0000

    Bug Fixes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@
 	
 	$(document).ready(function(){ 
 	"use strict";
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 			
 	// Global Variables and Functions ********************************************************
 	
@@ -55,6 +65,14 @@
 			addtoDelay(timeMed);
 		}
 		
+		function fraction(x,y) {
+			return (x / y);
+		}		
+		
+		function percentage(x,y) {
+			return Math.floor((x / y) * 100) + "%";
+		}
+		
 		function showStories() {
 			for ( i = 0, arrayLoop = $( ".story").toArray(); i < arrayLoop.length; i++ ) {
 				setTimeout(fadeInElement(arrayLoop[i]), delayTime);
@@ -66,20 +84,29 @@
 			return Math.floor(Math.random() * upper) + lower;	
 		}
 		
-		function togglePausePlay(player, state) {
+		function togglePausePlay(story, player, state) {
 			if (state === 0) {
 				player[0].pause();
+				$(story + ' .toggleState').html('&#9208;');
 			} else if (state === 1) {
 				player[0].play();
+				$(story + ' .toggleState').html('||');
 			} else {
 				if(player[0].paused) {
 					player[0].play();
+					$(story + ' .toggleState').html('||');
 				} else {
 					player[0].pause();
+					$(story + ' .toggleState').html('&#9658;');
 				}
 				 return false;
 			}
 		}
+		
+		
+		
+		
+		
 		
 		// Get Today's Date
 		
@@ -126,6 +153,16 @@
 		
 		var today = day + " " + date + " " + month + " " + year;
 			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 	// INTRO ********************************************************
 		
 		$('h1').delay(delayTime).fadeIn(timeMed, function() { 			// TITLE
@@ -163,6 +200,16 @@
 	
 		*/
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	// LARGE_MAP / MINI_MAP TRANSITION ********************************************************
 	
 		function mapTransition(e) {
@@ -186,24 +233,37 @@
 			mapTransition(this);
 		});
 		
-	// STORIES ********************************************************
+
+
+
+
+
+
+
+
+
+
 		
 		
 	// MEDIA PLAYER ********************************************************
 	function mediaPlayer(x) {
 		
 		if (typeof story !== 'undefined') {
-			togglePausePlay(player, 0); // 0 = make pause, 1 = make play, other = toggle
+			togglePausePlay(story, player, 0); // 0 = make pause, 1 = make play, other = toggle
 		}
+		
 		var story = "#" + $(x).attr("data-story");
-
 		var player = $(story+' .player');
-		var playerDuration =  player[0].duration;
-		
-		var playerCurrent = 0;
-		var	playerProgress = "";
+			var playerDuration =  player[0].duration;
+			var playerCurrent = 0;
+			var	playerProgress = "0%";
+			
+		var toggleState = $(story+' .toggleState');
 		var timeline = $(story+' .timeline');
-		
+		var scrubHead = $(story+' .timeline .scrubHead');
+		var scrubed = $(story+' .timeline .scrubed');
+		var timeIndicator = $(story+' .timeline .timeIndicator');
+		var scrollIndicator = $(story+' .scrollPosition');
 		var poiCat = $( story + " .poi" ).toArray();
 		
 		// Define Player Duration
@@ -219,19 +279,61 @@
 		
 		docHeight = $(document).height();
 		
-		//FUNCTIONS ----------------------------
 		
-		// Every time player updates
-		function timelineUpdate(t) { 
-			playerCurrent = player[0].currentTime;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//FUNCTIONS -----------------------------------------------------------------------
+				
+																			//CalculateTime
+		function playerTime(x) {
+			// window position		
+			x = (x - ($(timeline).offset().top)); 
+						
+			newCurrent = fraction(x,$(timeline).height()) * playerDuration;
+			return newCurrent;
+		}
+		
+		
+		
+		
+		
+
+																			//updatePlayer
+		function updatePlayer(newCurrent) {
+		
+			//Update progress bar and video currenttime
+			player[0].currentTime = newCurrent;
+			
+			newProgress = Math.floor((newCurrent / playerDuration) * 100);
+			timelineUpdate(newProgress);
+			
+			console.log("NEW PROGRESS: " + newProgress);
+			
+			return newProgress;
+
+		}
+		
+		
+		
+		
+		
+		
+		
+		// When player Updates Time, if value is parsed to it - it will be used to update timeline, otherwise the current time will be calculated
+		function timelineUpdate(newProgress) { 
 			
 			if (typeof t !== 'undefined') {
-				playerProgress = t;
+				playerProgress = newProgress;
 			} else {
-				playerProgress = ((playerCurrent / playerDuration) * 100) + "%";
-
-				timeline.find('.scrub_head').css({'top': playerProgress});
-				timeline.find('.scrub_head span').css({'height': playerProgress});
+				playerCurrent = player[0].currentTime;
+				playerProgress = Math.floor((playerCurrent / playerDuration) * 100) + "%";
 				
 				// Every time the player updates, loop through all POI's  
 				for ( i = 0; i < poiCat.length; i++ ) { 
@@ -253,36 +355,64 @@
 					}
 				} // End FOR			
 			}
-			timeline.find('.scrub_head').css({'top': playerProgress});
-			timeline.find('.scrub_head span').css({'height': playerProgress});	
+			$(scrubHead).css({'top': playerProgress});
+			$(scrubed).css({'height': playerProgress});	
 					
 		} // END timelineUpdate Function
 		
-		//CalculateTime
-		function playerTime(x) {
-	
-			// window position		
-			x = (x - scrollTop);  
-			// Y position of click on timeline, fraction
-			var timelineClick = x / timeline.height();
-			
-			//usable percentage can be applied to css attributes
-			newProgress = Math.floor(100 * timelineClick) + "%";
-			
-			// newCurrent time
-			newCurrent = timelineClick * playerDuration;
-			return newCurrent;
+		
+		
+		
+		
+		
+		
+		//Expand POI Function
+		function expandPOI(thisPOI) {
+			$(timeline).find('.time_indicator').addClass('activePOI');
+			$(timeline).find('.time_indicator').css({'top':(parseInt($(thisPOI).css('top')) - $(thisPOI).find('.poi_expand').height() / 2),'color':'rgba(255,255,255,1)'}).queue(function(next) {
+				$(thisPOI).addClass('poi_active').children().fadeIn(500);
+				next();
+			});	
+			$(thisPOI).find('.poi_expand').css({'border-top-left-radius':'0'});
+			lockIndicator = 1;
 		}
 		
-		//updatePlayer
-		function updatePlayer(x,y) {
-
-			scrollTop = $(window).scrollTop();	
-		
-			//Update progress bar and video currenttime
-			timelineUpdate(newProgress);
-			player[0].currentTime = newCurrent;	
+		//Contract POI Function
+		function contractPOI(thisPOI) {
+			$('.poi').removeClass('poi_active').children().fadeOut(500, function() {
+				$(thisPOI).find('.poi_expand').css({'border-top-left-radius':'20px'});
+				$(timeIndicator).removeClass('activePOI');
+				$(timeIndicator).css({'top':parseInt($(thisPOI).css('top'))});
+			});
+			lockIndicator = 0;
 		}
+		function updateIndicator(time) {
+			if (time === 0) {
+				$(timeIndicator).fadeOut(200);
+			} else {
+				if (lockIndicator === 0) {
+					$(timeIndicator).fadeIn(200);
+					$(timeIndicator).css({'top':percentage(time,playerDuration)});	
+				
+					// Format Time
+					var indicatorMinutes = Math.floor(time / 60);
+					var indicatorSeconds = Math.floor(time - (60 * indicatorMinutes));
+					
+					var indicatorTime = function() {
+						if (indicatorMinutes < 10) { indicatorMinutes = "0" + indicatorMinutes; }
+						if (indicatorSeconds < 10) { indicatorSeconds = "0" + indicatorSeconds; }
+						return indicatorMinutes + ":" + indicatorSeconds;
+					};
+					
+					$(timeIndicator).find('p').text(indicatorTime());			
+				}
+			}
+		}
+		
+		
+		
+		
+		
 		
 		
 		// TIME UPDATE
@@ -308,141 +438,154 @@
 		var timeDrag = false;
 		var newProgress = 0;
 		var newCurrent = 0;
+		var lockIndicator = 0;		
+		
+		
+		
+		
 		
 		
 		
 		
 		// ---------ON CLICK -------------
 		// Mouse Down, set timeDrag to true, updatePlayer
-		$(story + ' .timeline,' + story + ' .timeline span').mousedown(function(e) {
+		$(timeline).mousedown(function(e) {
 /**/		if (e.target !== this) return;
 				timeDrag = true;
-				updatePlayer(e.pageY);
+				updateIndicator(updatePlayer(playerTime(e.pageY)));
 		});
 		
 		
 		// Mouse Up, set timeDrag to false, updatePlayer
-		$(story + ' .timeline,' + story + ' .timeline span').mouseup(function(e) {
+		$(timeline, story + ' .timeline div').mouseup(function(e) {
 			if(timeDrag) {
 				timeDrag = false;
-				updatePlayer(e.pageY);
+				updatePlayer(playerTime(e.pageY));
 			}
+			updateIndicator(0); 
 		});
+		
+		
+		
+		
+		
 		
 		
 		
 		
 		// ---------ON DRAG -------------
 		// On drag, updatePlayer, double check target is target, update scrub head, indicator and time.
-		$(story + ' .timeline,' + story + ' .timeline span').mousemove(function(e) {
-		
+		$(timeline, story + ' .timeline div').mousemove(function(e) {
 		
 		if(timeDrag) {
-				updatePlayer(e.pageY);
+				updatePlayer(playerTime(e.pageY));
 			}			
 			
 /**/		if (e.target !== this) return;
 			
-			
 			// Update scrub
-			timeline.find('.scrub_head').css({'top':e.pageY});
-			timeline.find('.time_indicator').css({'top':newProgress});	
+			$(scrubHead).css({'top':e.pageY});	
 			
-			
-			// Format Time
-			var indicatorMinutes = Math.floor(playerTime(e.pageY) / 60);
-			var indicatorSeconds = Math.floor(playerTime(e.pageY) - (60 * indicatorMinutes));
-			var indicatorTime = function() {
-				if (indicatorMinutes < 10) { indicatorMinutes = "0" + indicatorMinutes; }
-				if (indicatorSeconds < 10) { indicatorSeconds = "0" + indicatorSeconds; }
-				return indicatorMinutes + ":" + indicatorSeconds;
-			};
-			timeline.find('.time_indicator p').text(indicatorTime());
+			updateIndicator(playerTime(e.pageY));		
 		});
+		
+		
+		
+		
+		
 		
 		
 		
 		
 		// ---------ON HOVER -------------
 		// mouseover check target is target, format scrub_head, and fade in indicator Time.
-		timeline.mouseenter(function(e) {
+		$(timeline).mouseenter(function(e) {
+			updateIndicator(playerTime(e.pageY));
 /**/		if (e.target !== this) return;
-				timeline.find('.scrub_head').css({'top':e.pageY});
-				timeline.find('.scrub_head').css({'top':newProgress});
-				timeline.find('.indicator_time').fadeIn(1000);
+				$(scrubHead).css({'top':newProgress});
 		});
 		
 		
 		// mouse leave, Set scrub_head back to current time, fade out indicator time 
-		timeline.mouseleave(function() {
+		$(timeline).mouseleave(function() {
 			timeDrag = false;
 			timelineUpdate();
-			timeline.find('.indicator_time').fadeOut(200);
-			
+			updateIndicator(0);
+				$(scrubHead).css({'top':0});
 		});
 		
 		
 		$('.poi').css({'opacity':'1'}).hover(function() {
-			timeline.find('scrub_head').css({'opacity':'0'});
+			$(scrubHead).css({'opacity':'0'});
 		});
 		
 		
 		//MOUSEENTER: POI_EXPAND
 		$('.poi, .poi_expand, .time_indicator, p').mouseenter(function() {
 			timeDrag = false;
+			$(scrubHead).css({'opacity':'0'});
+			$(timeIndicator).fadeOut(200);	
 		});
 		
 		
+		
+		
+		
+		
+		
 		// CLICK: POI
-		//
-		function expandPOI(thisPOI) {
-			$(timeline).find('.time_indicator').css({
-				'border-bottom-left-radius':'0',
-				'transition':'0.5s',
-				'background-color':'rgba(225,229,202,1.00)',
-				'top':(parseInt($(thisPOI).css('top')) - ($(thisPOI).find('.poi_expand').height() / 2) - 50),'color':'rgba(255,255,255,1)'
-			}).queue(function(next) {
-				$(thisPOI).addClass('poi_active').children().fadeIn(500);
-				next();
-			});	
-			$(thisPOI).find('.poi_expand').css({'border-top-left-radius':'0'});
-		}
-		
-		function contractPOI(thisPOI) {
-			$('.poi').removeClass('poi_active').children().fadeOut(500, function() {
-				$(thisPOI).find('.poi_expand').css({'border-top-left-radius':'20px'});
-				$(timeline).find('.time_indicator').css({'border-bottom-left-radius':'20px','transition':'0.5s', 'background-color':'rgba(0,0,0,0.35)', 'top':parseInt($(thisPOI).css('top'))});
-			});
-		}
-		
-		
+		// On Click
 		$('.poi').click(function(e) {
 			var thisPOI = this;
 			if (timeDrag !== true) { 			
 /**/			if (e.target !== this) return; // Only continue if click element is exact element (no children)
-
+					
+					//Is POI already expanded?
 					if ( $(thisPOI).hasClass('poi_active')) {
+						
+						// If so, contract it
 						contractPOI(thisPOI);						
 					} else {
+						
+						//Otherwise expand it
 						$('.poi').removeClass('poi_active').children().fadeOut(500);
-							
+						
+						//If there isn't enough room above POI
 						if ( (($(thisPOI).find('.poi_expand').height() / 2) + 50	) > parseInt($(this).css('top')) ) {
 							
+							//Push POI to 20px below top
 							$(thisPOI).find('.poi_expand').css({'top': '20px'});
 							expandPOI(thisPOI);
-							
+
+						//If there isn't enough room below POI
 						} else if ( (($(this).find('.poi_expand').height() / 2) + 50) > ( winHeight - parseInt($(this).css('top'))) ) {
 							
+							//Push POI to 20px above bottom
 							$(thisPOI).find('.poi_expand').css({'top':(winHeight - $(this).height() - 20)+"px"});
 							expandPOI(thisPOI);	
 													
 						} else {
+							
+							//If there is enough space, expand POI.
 							expandPOI(thisPOI);
 						}
 						
 					}
 			}
 		});
+		
+		
+		
+		
+			$(window).on('scroll', function () {
+		
+				// Timeline Scroll Position
+				var scrollPosition = scrollTop / (docHeight - winHeight + 9);
+				var scrollProgress = 100 * (scrollPosition) + "%";
+				$(scrollIndicator).css({'top': scrollProgress});
+				console.log(scrollProgress);
+			});
+		
 		
 		
 		//CLICK: POI_EXPAND
@@ -462,7 +605,20 @@
 			}
 		});
 	
+		// MEDIA CONTROLS
+		
+		$(toggleState).click(function() {
+			togglePausePlay(story, player); // 0 = make pause, 1 = make play, other = toggle
+		});
+
+	
 	} // END mediaPlayer Function
+	
+	
+	
+	
+	
+	
 	
 	
 	//CLICK: LOADPLAYER - INITIATE MEDIAPLAYER
@@ -474,50 +630,55 @@
 		}
 	});
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	// ANIMATIONS ------------------------------------
 		
 		var scrollStart = 0;
-		var scrollEnd = 1000;	
+		var scrollEnd = 10000;
 		var scrollEnabled = 0;
 		
-		if (scrollEnabled === 1) {
-			$('body').css({'height':(winHeight + scrollEnd)});
-		}
-		
 		$(window).on('scroll', function () {
-				
-			// Map Transition
-			var sProgress = (scrollTop - scrollStart) / (scrollEnd - scrollStart);
-						
-			if (sProgress > 1) { sProgress = 1; }
-			else if (sProgress < 0) { sProgress = 0; }
-			
-			/*
-			if (sProgress > 0.01) {
-				$('span.2day').fadeOut(timeShort);	
-			}
-			*/
-			
+	
 			if (scrollEnabled === 1) {
-				$('.old_map').css({'opacity': sProgress});
+				$('body').css({'height':(winHeight + scrollEnd)});
+								
+				// Map Transition
+				var sProgress = (scrollTop - scrollStart) / (scrollEnd - scrollStart);
+							
+				if (sProgress > 1) { sProgress = 1; }
+				else if (sProgress < 0) { sProgress = 0; }
+				
+				/*
+				if (sProgress > 0.01) {
+					$('span.2day').fadeOut(timeShort);	
+				}
+				*/
+				if (scrollEnabled === 1) {
+					$('.old_map').css({'opacity': sProgress});
+				}
+						
+				if (sProgress === 1) {
+						$('.new_map').remove();	
+						$('.date').fadeOut(timeShort);
+						$('body').css({'height':'auto'});
+						scrollEnabled = 0;
+						showStories();
+				}
 			}
-					
-			if (sProgress === 1) {
-					$('.new_map').remove();	
-					$('.date').fadeOut(timeShort);
-					$('body').css({'height':'auto'});
-					scrollEnabled = 0;
-					showStories();
-			}
-			
-			// Timeline Scroll Position
-			var scrollPosition = scrollTop / (docHeight - winHeight + 9);
-			var scrollProgress = 100 * (scrollPosition) + "%";
-			$('.scroll_position').css({'top': scrollProgress});
-			
-			
 			
 		});
 
+			
+			
+			
 	// DOCUMENT READY
 	});
+	
