2e382478c8dd4cd8c5b7637b6093dc0b80d6f8d1
Jamie De Vivo (i7436295) <i7436295@mac38570.student.bournemouth.ac.uk>
2018-10-24 15:57:33 +0100

    Seperate into includes

---
+++ includes/head.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+	<head>        
+        <!-- Meta Data -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="keywords" content="Bournemouth, Bombings, Air Raid, 1942, 23rd May, Sunday, Local, World War Two, 2, Bournemouht University, Jamie De Vivo">
+        <meta name="description" content="The story of the bombing of Bournemouth on Sunday 23rd May 1943, told from the perspective of people who expireanced it first hand. An Interactive documentary that doesn't just tell you facts, but immerses you into the story.">
+        
+		<title>Bombing of Bournemouth | Sunday 23rd May 1943</title>
+
+        <!-- Bootstrap Styles -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+		<!-- Custom Styles -->
+        <link href="css/main.css" rel="stylesheet" type="text/css">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Clicker+Script" rel="stylesheet">
+		
+	</head>
+    
+    <body> 
--- main.php
+++ main.php
@@ -1,28 +1,9 @@
-<!doctype html>
-<html>
-	<head>        
-        <!-- Meta Data -->
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <meta name="keywords" content="Bournemouth, Bombings, Air Raid, 1942, 23rd May, Sunday, Local, World War Two, 2, Bournemouht University, Jamie De Vivo">
-        <meta name="description" content="The story of the bombing of Bournemouth on Sunday 23rd May 1943, told from the perspective of people who expireanced it first hand. An Interactive documentary that doesn't just tell you facts, but immerses you into the story.">
-        
-        
-		<title>Bombing of Bournemouth | Sunday 23rd May 1943</title>
-
-        <!-- Bootstrap Styles -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
-		<!-- Custom Styles -->
-        <link href="css/main.css" rel="stylesheet" type="text/css">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Clicker+Script" rel="stylesheet">
-		
-	</head>
-    
-    <body> 
-    
-    <!-- CONTENT --------------------------------------------------------------------------------------------------------------------------------------------->
+<?
+    require_once 'core/init.php';
+    $title = "Home";
+    require 'includes/head.php';
+?>
+<!-- CONTENT --------------------------------------------------------------------------------------------------------------------------------------------->
     	<div id="container">
 			<ul>
 				<li><a href="#" class="loadPlayer" data-story="story1">Vernon's Story</a></li>
@@ -690,16 +671,6 @@
 			<h1>Bombing of Bournemouth | Sunday 23rd May 1943</h1>
 			<p class="prompt">Pick a Story to learn more.</p>
 	</div>
-    
-    <!-- END CONTENT --------------------------------------------------------------------------------------------------------------------------------------------->
-	<link href="https://fonts.googleapis.com/css?family=Enriqueta|Gentium+Book+Basic:400i|Ramaraja" rel="stylesheet">      
-    <!-- Bootstrap Scripts -->
-    <script src="js/jquery.js"></script>
-    <script src="js/tether.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    
-    <!-- Custom Scripts -->
-    <script src="js/script.js"></script>
-   
-    </body>
-</html>
+<?
+    require 'includes/foot.php';
+?>
